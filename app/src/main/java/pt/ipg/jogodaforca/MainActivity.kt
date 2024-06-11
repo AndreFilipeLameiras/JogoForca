632801c66bb0d9facbd580d6abe6816cb15fd2a6
Andre <afplameiras@gmail.com>
2024-06-11 11:39:33 +0100

    Alterada a caixa de texto onde se escreve a letra

--- app/src/main/java/pt/ipg/jogodaforca/MainActivity.kt
+++ app/src/main/java/pt/ipg/jogodaforca/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +15,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +31,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pt.ipg.jogodaforca.ui.theme.JogoDaForcaTheme
@@ -103,7 +110,13 @@ fun ForcaWithButtonAndImage(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        BasicTextField(
+        EditTexField(
+            label = R.string.letra,
+            leadingIcon = R.drawable.textfield,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
             value = input,
             onValueChange = { input = it.take(1) },
             modifier = Modifier
@@ -152,4 +165,27 @@ fun GreetingPreview() {
     JogoDaForcaTheme {
         ForcaWithButtonAndImage()
     }
+}
+
+
+@Composable
+fun EditTexField(
+    @StringRes label: Int,
+    @DrawableRes leadingIcon: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier) {
+
+    TextField(
+        value = value,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
+        onValueChange = onValueChange,
+        label = { Text(stringResource(label))},
+        singleLine = true,
+        keyboardOptions = keyboardOptions,
+        modifier = modifier
+
+    )
+
 }
