3a5b8e1dfa997b418af1a2e9f600083d33913f1a
Andre <afplameiras@gmail.com>
2024-06-11 09:58:22 +0100

    Criaçao da infraestrutura do layout

--- app/src/main/java/pt/ipg/jogodaforca/MainActivity.kt
+++ app/src/main/java/pt/ipg/jogodaforca/MainActivity.kt
@@ -1,13 +1,16 @@
 package pt.ipg.jogodaforca
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pt.ipg.jogodaforca.ui.theme.JogoDaForcaTheme
@@ -17,20 +20,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JogoDaForcaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ForcaWithButtonAndImage(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ForcaWithButtonAndImage(modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -41,6 +40,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JogoDaForcaTheme {
-        Greeting("Android")
+        ForcaWithButtonAndImage()
     }
 }
